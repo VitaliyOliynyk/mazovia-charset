be986a8d48a2413e62a7a43e6c093ecafc48068e
Vitaliy Oliynyk <xaoc00@gmail.com>
2012-03-08 16:59:27 +0100

    add test class

--- src/main/java/eu/vitaliy/pl/charset/DOSCharsetProvider.java
+++ src/main/java/eu/vitaliy/pl/charset/DOSCharsetProvider.java
@@ -15,7 +15,7 @@ public class DOSCharsetProvider extends CharsetProvider {
 
     public static final String MAZOVIA_CHARSET_NAME = "CP-896";
     public static final String LATIN_2_CHARSET_NAME = "CP-852";
-    private final static String[] mazoviaAliases = new String[]{
+    public final static String[] MAZOVIA_ALIASES = new String[]{
         "Mazovia",
         "Mazovia",
         "mazovia",
@@ -38,7 +38,7 @@ public class DOSCharsetProvider extends CharsetProvider {
     public Iterator<Charset> charsets() {
         if (charsets == null || charsets.isEmpty()) {
             charsets = new ArrayList<Charset>(2);
-            charsets.add(new MazoviaCharset(MAZOVIA_CHARSET_NAME, mazoviaAliases));
+            charsets.add(new MazoviaCharset(MAZOVIA_CHARSET_NAME, MAZOVIA_ALIASES));
             charsets.add(new IBMLatinCharset(LATIN_2_CHARSET_NAME, null));
         }
 
@@ -48,9 +48,9 @@ public class DOSCharsetProvider extends CharsetProvider {
     @Override
     public Charset charsetForName(String charsetName) {
 
-        List<String> mazoviaList = Arrays.<String>asList(mazoviaAliases);
+        List<String> mazoviaList = Arrays.<String>asList(MAZOVIA_ALIASES);
         if (mazoviaList.contains(charsetName.trim())) {
-            return new MazoviaCharset(MAZOVIA_CHARSET_NAME, mazoviaAliases);
+            return new MazoviaCharset(MAZOVIA_CHARSET_NAME, MAZOVIA_ALIASES);
         } else if (charsetName.trim().equalsIgnoreCase(LATIN_2_CHARSET_NAME)) {
             return new IBMLatinCharset(LATIN_2_CHARSET_NAME, null);
         } else {
---
+++ src/test/java/eu/vitaliy/pl/charset/DOSCharsetProviderTest.java
@@ -0,0 +1,24 @@
+package eu.vitaliy.pl.charset;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.fest.assertions.Assertions.*;
+/**
+ * Created by IntelliJ IDEA.
+ * User: xaoc
+ * Date: 08.03.12
+ * Time: 16:55
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class DOSCharsetProviderTest {
+
+    @Test
+    @Parameters()
+    public void charsetForNameTest(){
+
+    }
+}
